dfac0ce12d507d3ae014b88e4931a25dc8de03a1
Loic CHETY <lchety@e2r12p13.42.fr>
2019-06-18 17:47:05 +0200

    wip

--- firebase/public/app.js
+++ firebase/public/app.js
@@ -1,20 +1,28 @@
 document.addEventListener("DOMContentLoaded", event => {
-
 	const app = firebase.app();
+
+	const db = firebase.firestore();
+
+	const myPost = db.collection('posts').doc('firstpost');
+
 	console.log(app);
 
+	myPost.get()
+		.then(doc => {
+			const data = doc.data();
+			document.write( data.title + `<br>`)
+			document.write( data.createdAt )
+		})
 });
-
 
 function googleLogin() {
 	const provider = new firebase.auth.GoogleAuthProvider();
 
 	firebase.auth().signInWithPopup(provider)
-
-		.then(result => {
+		.then( result => {
 			const user = result.user;
-			document.write('Hello ${user.displayName}');
+			document.write(`Hello ${user.displayName}`);
 			console.log(user);
 		})
-		.catch(console.log);
+		.catch(console.log)
 }
